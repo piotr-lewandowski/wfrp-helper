66a51f2b47a8ccdcc708a10bb58ddbced16ee586
MisterSadness <lewandowskipiotr98@gmail.com>
2020-11-06 00:25:42 +0100

    Implemented configuration for Entities DSL

--- lib/Language/Entity.hs
+++ lib/Language/Entity.hs
@@ -4,47 +4,62 @@ import Language.Dice
 import Language.Main
 import Model.Entity
 import Model.Modifier
+import Lens.Simple
 
 data EntityExpression = 
     NameExpression Entity |
-    ModifierExpression Modifier |
+    ModifierExpression Modifier String |
     AndExpression EntityExpression EntityExpression |
     QuantityExpression DiceExpression EntityExpression
 
 instance Show EntityExpression where
-    show (NameExpression _) = "Name"
-    show (ModifierExpression _) = "Modifier"
+    show (NameExpression e) = e^.name
+    show (ModifierExpression _ _) = "Modifier"
     show (AndExpression a b) = "And (" ++ show a ++ " " ++ show b ++ ")"
     show (QuantityExpression d e) = "Quantity (" ++ show d ++ " " ++ show e ++ ")"
 
+modifiers :: [(String, Modifier)]
+modifiers = [("warrior", warrior), ("archer", archer), ("captain", captain)]
+
+entities :: [(String, Entity)]
+entities = [("human", human), ("elf", elf), ("dwarf", dwarf)]
+
 entityExpression :: Parser EntityExpression
-entityExpression = choice [andExpression, quantityExpression, singleEntity]
+entityExpression = choice [a, q, s]
+    where
+        a = andExpression s
+        q = quantityExpression s
+        s = singleEntity m n
+        m = choice $ map modifierExpression modifiers
+        n = choice $ map nameExpression entities
 
-singleEntity :: Parser EntityExpression
-singleEntity = try $ do
-    mods <- modifierExpression `endBy` spaces
-    (NameExpression ent) <- nameExpression
-    let modifiers = map (\(ModifierExpression m) -> m) mods
-    return $ NameExpression $ foldr ($) ent modifiers
+singleEntity :: Parser EntityExpression -> Parser EntityExpression -> Parser EntityExpression
+singleEntity m n = try $ do
+    modExpsPre <- m `endBy` spaces
+    (NameExpression ent) <- n
+    _ <- space
+    modExpsPost <- m `endBy` spaces
+    let mods = map (\(ModifierExpression x _) -> x) $ modExpsPre ++ modExpsPost
+    return $ NameExpression $ foldr ($) ent mods
 
-modifierExpression :: Parser EntityExpression
-modifierExpression = try $ do
-    _ <- string "modifier"
-    return $ ModifierExpression id
+modifierExpression :: (String, Modifier) ->  Parser EntityExpression
+modifierExpression (s, m) = try $ do
+    _ <- string s
+    return $ ModifierExpression m s
 
-nameExpression :: Parser EntityExpression
-nameExpression = try $ do
-    n <- string "name"
-    return $ NameExpression $ human { name = n }
+nameExpression :: (String, Entity) -> Parser EntityExpression
+nameExpression (s, e) = try $ do
+    _ <- string s
+    return $ NameExpression e
 
-andExpression :: Parser EntityExpression
-andExpression = try $ do
-    a <- choice [singleEntity, quantityExpression]
+andExpression :: Parser EntityExpression -> Parser EntityExpression
+andExpression entityParser = try $ do
+    a <- choice [entityParser, quantityExpression entityParser]
     _ <- spaces
     _ <- string "and"
     _ <- spaces
-    b <- choice [andExpression, singleEntity, quantityExpression]
+    b <- choice [andExpression entityParser, entityParser, quantityExpression entityParser]
     return $ AndExpression a b
 
-quantityExpression :: Parser EntityExpression
-quantityExpression = try $ QuantityExpression <$> diceExpression <*> (spaces *> singleEntity)
+quantityExpression :: Parser EntityExpression -> Parser EntityExpression
+quantityExpression entity = try $ QuantityExpression <$> diceExpression <*> (spaces *> entity)
