d5395836e5ed43cc002cfc859185c39ef3de4fc8
MisterSadness <lewandowskipiotr98@gmail.com>
2020-12-18 04:55:51 +0100

    Implemented a simple REPL in Main.hs

---
+++ lib/Language/Repl.hs
@@ -0,0 +1,21 @@
+module Language.Repl where
+
+import Language.Main
+import Language.Dice
+import Language.Entity
+
+quitString :: Parser String
+quitString = try $ choice $ map string ["quit", "exit"]
+
+data TopLevelExpression = TlDice DiceExpression | TlEntity EntityExpression | TlQuit QuitExpression
+newtype QuitExpression = QuitExpression String
+
+topLevelP :: Parser TopLevelExpression
+topLevelP = choice [ 
+    TlQuit . QuitExpression <$> quitString
+    , TlDice <$> diceExpression
+    , TlEntity <$> entityExpression 
+    ]
+
+parseTopLvl :: String -> Either ParseError TopLevelExpression
+parseTopLvl = parse topLevelP ""
