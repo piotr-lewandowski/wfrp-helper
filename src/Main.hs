061f66b1cc59c1da4f94b4dd4d9d104202bb0a8c
Piotr Lewandowski <47465630+MisterSadness@users.noreply.github.com>
2021-01-14 14:00:34 +0100

    Feature/main repl (#21)
    * Added hie.yaml
    
    * Implemented a simple REPL in Main.hs
    
    * Improved entity printing
    
    * Fixed tests

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import Language.Main
+import Language.Repl
+import Language.Dice
+import Language.Entity
+import System.IO
+import Printing
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = parseLine
+
+parseLine :: IO ()
+parseLine = do
+  putStr "> " 
+  hFlush stdout
+  str <- getLine
+  result <- showResult $ parseTopLvl str
+  printOrQuit result
+
+showResult :: Either ParseError TopLevelExpression -> IO (Maybe String)
+showResult res = case res of
+  Left err -> return $ Just $ show err
+  Right topLvl -> case topLvl of
+    TlQuit _ -> return Nothing
+    TlDice d -> Just . show <$> eval d
+    TlEntity e -> Just . printTable <$> evalEntity e
+
+printOrQuit :: Maybe String -> IO ()
+printOrQuit (Just s) = putStrLn s >> parseLine
+printOrQuit Nothing = return ()
+
+
+
